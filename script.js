25c0c5b46f400ebf444b85dd349997ef3e319a33
sahebkumarsahni0-source <sahebkumarsahni0@gmail.com>
2026-08-14 21:46:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function goToShayari() {
+
+  console.log("JAI HIND clicked");
+
+  window.location.href = "shayari.html";
+}
+
+
+function goToPhoto() {
+
+  console.log("Next page clicked");
+
+  window.location.href = "photo.html";
+}
+
+
+function goHome() {
+
+  console.log("Home clicked");
+
+  window.location.href = "index.html";
+}
